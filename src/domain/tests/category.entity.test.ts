a2f0d2bded62efc55213cec8b6a32db5af646f11
dudow <edu30.nm@gmail.com>
2025-04-04 18:23:14 +0000

    feat: add fist test

---
+++ src/domain/tests/category.entity.test.ts
@@ -0,0 +1,78 @@
+import { Category } from "../category.entity";
+
+describe('Category Unit Tests', () => {
+    test("Constructor", () => {
+        let category = new Category({
+            name: "Movie",
+            is_active: true,
+            created_at: new Date()
+        });
+
+        expect(category.category_id).toBeUndefined();
+        expect(category.name).toBe("Movie");
+        expect(category.description).toBeNull();
+        expect(category.is_active).toBe(true);
+        expect(category.created_at).toBeInstanceOf(Date);
+
+        category = new Category({
+            name: "Movie",
+        });
+
+        expect(category.category_id).toBeUndefined();
+        expect(category.name).toBe("Movie");
+        expect(category.description).toBeNull();
+        expect(category.is_active).toBe(true);
+        expect(category.created_at).toBeInstanceOf(Date);
+    })
+})
+
+describe("Create and change commands", () => {
+    test("Should create a new category", () => {
+        const category = Category.create({
+            name: "Movie",
+        })
+
+        expect(category.name).toBe("Movie");
+        expect(category.description).toBeNull();
+        expect(category.is_active).toBe(true);
+        expect(category.created_at).toBeInstanceOf(Date);
+    })
+
+    test("Should create a new category and change name", () => {
+        const category = Category.create({
+            name: "Movie",
+        })
+
+        category.changeName("Movies");
+
+        expect(category.name).toBe("Movies");
+    })
+
+    test("Should create a new category and change description", () => {
+        const category = Category.create({
+            name: "Movie",
+        })
+
+        category.changeDescription("New description");
+
+        expect(category.description).toBe("New description");
+    })
+
+    test("Should create a new category and change active", () => {
+        const category = Category.create({
+            name: "Movie",
+            is_active: true
+        })
+
+        expect(category.is_active).toBe(true);
+
+        category.deactivate();
+
+        expect(category.is_active).toBe(false);
+
+        category.activate();
+
+        expect(category.is_active).toBe(true);
+    })
+
+})
