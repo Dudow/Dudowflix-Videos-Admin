c71aeaa23bccac898f35b71c0b91bef1f55f14ff
dudow <edu30.nm@gmail.com>
2025-04-07 17:33:16 +0000

    feat: add uuid into category and make tests

--- src/category/domain/tests/category.entity.test.ts
+++ src/category/domain/tests/category.entity.test.ts
@@ -1,3 +1,4 @@
+import { Uuid } from "../../../shared/domain/value-objects/uuid.vo";
 import { Category } from "../category.entity";
 
 describe('Category Unit Tests', () => {
@@ -8,7 +9,7 @@ describe('Category Unit Tests', () => {
             created_at: new Date()
         });
 
-        expect(category.category_id).toBeUndefined();
+        expect(category.category_id).toBeInstanceOf(Uuid);
         expect(category.name).toBe("Movie");
         expect(category.description).toBeNull();
         expect(category.is_active).toBe(true);
@@ -18,7 +19,7 @@ describe('Category Unit Tests', () => {
             name: "Movie",
         });
 
-        expect(category.category_id).toBeUndefined();
+        expect(category.category_id).toBeInstanceOf(Uuid);
         expect(category.name).toBe("Movie");
         expect(category.description).toBeNull();
         expect(category.is_active).toBe(true);
@@ -75,4 +76,23 @@ describe("Create and change commands", () => {
         expect(category.is_active).toBe(true);
     })
 
+})
+
+describe("category_id field", () => {
+    const arrange = [
+        {category_id: null}, {category_id: undefined}, {category_id: new Uuid},
+    ]
+
+    test.each(arrange)("id = %j", ({category_id}) => {
+        const category = new Category({
+            name: "Movie",
+            category_id: category_id as any
+        })
+
+        expect(category.category_id).toBeInstanceOf(Uuid);
+
+        if(category_id instanceof Uuid){
+            expect(category.category_id.id).toBe(category_id.id);
+        }
+    })
 })
